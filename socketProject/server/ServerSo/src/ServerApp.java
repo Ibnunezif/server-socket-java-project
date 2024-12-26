5ddf43dc3f42920b62c1cb5c54d0f4f67b219786
Abdulbasit Nezif Abasambi <142674760+Ibnunezif@users.noreply.github.com>
2024-12-26 20:32:17 +0300

    Update ServerApp.java
    I have added thread to read and write concurrently

--- socketProject/server/ServerSo/src/ServerApp.java
+++ socketProject/server/ServerSo/src/ServerApp.java
@@ -1,29 +1,42 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ServerApp {
-    static DataInputStream in;
+class ReadMessage extends Thread {
+    Socket socket;
+    public ReadMessage(Socket socket) {
+        this.socket = socket;
+    }
+    DataInputStream in;
+    public void run() {
+        try {
+            in = new DataInputStream(socket.getInputStream());
+            while (true) {
+                String clientMessage = in.readUTF();
+                System.out.println("user: " + clientMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class WriteMessage extends Thread {
+    Socket socket;
+    public WriteMessage(Socket socket) {
+        this.socket = socket;
+    }
     static DataOutputStream out;
     static Scanner scanner = new Scanner(System.in);
-    
-    public static void main(String[] args) {
+
+    public void run() {
         try {
-            ServerSocket serverSocket = new ServerSocket(8000);
-            System.out.println("Server is listening on port 8000...");
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected.");
-
-            in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
-
             while (true) {
-                String receivedMessage = in.readUTF();
-                System.out.println("user: " + receivedMessage);
-
-                System.out.print("you: ");
+                // System.out.print("you: ");
                 String str = scanner.nextLine();
                 out.writeUTF(str);
                 out.flush();
@@ -31,5 +44,17 @@ public class ServerApp {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+}
+
+public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8000);
+        Socket socket = serverSocket.accept();
+
+        ReadMessage rm = new ReadMessage(socket);
+        WriteMessage wm = new WriteMessage(socket);
+        rm.start();
+        wm.start();
     }
 }
