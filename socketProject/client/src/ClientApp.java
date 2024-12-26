12bc206911e8b0d08dd26e65b6bda5f938eea23e
Abdulbasit Nezif Abasambi <142674760+Ibnunezif@users.noreply.github.com>
2024-12-26 20:31:02 +0300

    Update ClientApp.java
    I have added the thread to control concurrent read and write.

--- socketProject/client/src/ClientApp.java
+++ socketProject/client/src/ClientApp.java
@@ -1,30 +1,60 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientApp {
-    static DataInputStream in;
-    static DataOutputStream out;
-    static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        try {
-            Socket socket = new Socket("localhost", 8000);
-            out = new DataOutputStream(socket.getOutputStream());
-            in = new DataInputStream(socket.getInputStream());
-
-            while (true) {
-                System.out.print("you: ");
-                String str = scanner.nextLine();
-                out.writeUTF(str);
-                out.flush(); 
-
-                String clientMassege=in.readUTF();
-                System.out.println ("user :"+clientMassege);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+class ReadMessage extends Thread {
+    Socket socket;
+    public ReadMessage (Socket socket){
+        this.socket=socket;
     }
+        DataInputStream in;
+        public void run (){
+            try {
+                in = new DataInputStream(socket.getInputStream());
+                while (true) {
+                    String clientMassege=in.readUTF();
+                    System.out.println ("user :"+clientMassege);
+                }
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+}
+
+class WriteMessage extends Thread {
+    Socket socket;
+    public WriteMessage  (Socket socket){
+        this.socket=socket;
+    }
+     static DataOutputStream out;
+     static Scanner scanner = new Scanner(System.in);
+ 
+     public void run() {
+         try {
+             
+             out = new DataOutputStream(socket.getOutputStream());
+ 
+             while (true) {
+                 String str = scanner.nextLine();
+                 out.writeUTF(str);
+                 out.flush(); 
+             }
+         } catch (Exception e) {
+             e.printStackTrace();
+         }
+     }
+}
+
+public class ClientApp {
+   public static void main (String [] args) throws Exception {
+    Socket socket = new Socket("localhost", 8000);
+    ReadMessage rm= new ReadMessage(socket);
+    WriteMessage wm=new WriteMessage(socket);
+    rm.start();
+    wm.start();
+   }
 }
